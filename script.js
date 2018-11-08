d4469bf62e981d78e699a313d78f3f9b8522d7ce
Berkley Horan <horans11@gmail.com>
2018-11-08 10:06:16 -0700

    score counter

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let coursecollection;
-var numplayers = 5;
+var numplayers = 4;
 let numholes = 18;
 var courseId = 11819;
 var thisteetype = 1;
+var p1score = [];
+var p2score = [];
+var p3score = [];
+var p4score = [];
 var yards = [];
 var par = [];
 var hcp = [];
@@ -34,6 +38,7 @@ function loadCourse(courseId) {
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+
             $(".teeselect").children().remove()
             mycourse = JSON.parse(this.responseText);
             console.log(mycourse);
@@ -46,7 +51,9 @@ function loadCourse(courseId) {
             for(let i = 0; i < teeArray.length; i++){
                 $('.teeselect').append(`<option value="${i}"> ${teeArray[i].teeType}</option>`);
             }
+
             $('.teeselect').show(100)
+
             yards = [];
             par = [];
             hcp = [];
@@ -69,7 +76,8 @@ function loadCourse(courseId) {
 
 function chooseTee(value){
     thisteetype = value;
-    loadCourse($(".courses").val());
+    loadCourse($(".courses :selected").val());
+    loadCourse($(".courses :selected").val());
 }
 
 function buildcard() {
@@ -86,12 +94,65 @@ function buildcard() {
 function addholes() {
     for(p = 1; p <= numplayers; p++){
         for(h = 1; h <= numholes; h++){
-        $("#col" + h).append(`<span><p>Player ${p}</p><input class="hole" type="text" id=p${p}h${h} /></span>`);
-    }
-
-}}
+        $("#col" + h).append(`<span><p id="player${p}">Player ${p} : </p><input class="hole" type="text" id="p${p}h${h}" onchange="updatescore(this);"/></span>`);
+    }}
+}
 function choosePlayer(players){
     numplayers = players;
     buildcard();
 }
+function updatescore(value){
+    for(p = 1; p <= numplayers; p++){
+        for(h = 1; h <= numholes; h++){
+            let inputid = $("#p1h" + h).attr('id')
+            let input1 = $("#p1h" + h).val()
+            if(value.id == inputid){
+            p1score.push(input1);
+            }
+        
+            let input2 = $("#p2h" + h).val();
+            let input2id = $("#p2h" + h).attr('id')
+            if(value.id == input2id){
+            p2score.push(input2);
+            }
 
+
+            let input3 = $("#p3h" + h).val();
+            let input3id = $("#p3h" + h).attr('id')
+            if(value.id == input3id){
+            p3score.push(input3);
+            }
+
+            let input4 = $("#p4h" + h).val();
+            let input4id = $("#p4h" + h).attr('id')
+            if(value.id == input4id){
+            p4score.push(input4);
+            }
+            
+        }
+
+    }
+
+             var total = 0;
+            for (var e = 0; e < p1score.length; e++) {
+                total += p1score[e] << 0;
+            }
+            var total2 = 0;
+            for (var q = 0; q < p2score.length; q++) {
+                total2 += p2score[q] << 0;
+            }
+            var total3 = 0;
+            for (var k = 0; k < p3score.length; k++) {
+                total3 += p3score[k] << 0;
+            }
+            var total4 = 0;
+            for (var r = 0; r < p4score.length; r++) {
+                total4 += p4score[r] << 0;
+            }
+
+
+    $("#1").text(total/numplayers)
+    $("#2").text(total2/numplayers)
+    $("#3").text(total3/numplayers)
+    $("#4").text(total4/numplayers)
+}
